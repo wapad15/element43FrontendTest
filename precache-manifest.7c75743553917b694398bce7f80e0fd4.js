a60860d00cc5bbdea5443d774184a37a9dd3baa7
wapad <trabajorwilder@hotmail.com>
2019-09-26 06:33:29 -0500

    Updates

---
+++ precache-manifest.7c75743553917b694398bce7f80e0fd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f6abd56e2ad94ee43d6d58ad20bf7e",
+    "url": "/element43FrontendTest/index.html"
+  },
+  {
+    "revision": "90ae799d78205eae08cd",
+    "url": "/element43FrontendTest/static/css/main.129941c1.chunk.css"
+  },
+  {
+    "revision": "fb840b08d0b326372f43",
+    "url": "/element43FrontendTest/static/js/2.e5d08bb9.chunk.js"
+  },
+  {
+    "revision": "90ae799d78205eae08cd",
+    "url": "/element43FrontendTest/static/js/main.2c5b9873.chunk.js"
+  },
+  {
+    "revision": "9a112a5fb15ed656f1f7",
+    "url": "/element43FrontendTest/static/js/runtime-main.947dd74d.js"
+  }
+]);
